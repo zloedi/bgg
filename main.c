2836025c004d583291ccd4dc736b62aea5bfa317
zloedi <stoiko.todorov@gmail.com>
2016-06-02 17:37:50 +0300

    removed redundant start routine

--- main.c
+++ main.c
@@ -6,9 +6,6 @@ static void X_RegisterVars_f( void ) {
 static void X_Init_f( void ) {
 }
 
-static void X_Start_f( void ) {
-}
-
 static void X_Frame_f( void ) {
 }
 
@@ -22,7 +19,6 @@ int main( int argc, char *argv[] ) {
                colorrgb( 0.1f, 0.1f, 0.1f ),
                X_RegisterVars_f,
                X_Init_f,
-               X_Start_f,
                X_Frame_f,
                X_Done_f );
     return 0;
