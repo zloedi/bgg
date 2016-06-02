d3d9589c3a1ae95233e4b5bd3bb16908c86406fd
zloedi <stoiko.todorov@gmail.com>
2016-06-02 17:06:48 +0300

    multiproject update.

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
-#include <zhost.h>
+#include "zhost.h"
+
+static void X_RegisterVars_f( void ) {
+}
+
+static void X_Init_f( void ) {
+}
+
+static void X_Start_f( void ) {
+}
+
+static void X_Frame_f( void ) {
+}
+
+static void X_Done_f( void ) {
+}
 
 int main( int argc, char *argv[] ) {
     UT_RunApp( NULL, 
-               NULL,
-               NULL,
+               "bgg",
+               "Bullets, Grit, Gasoline",
                colorrgb( 0.1f, 0.1f, 0.1f ),
-               NULL,
-               NULL,
-               NULL,
-               NULL,
-               NULL );
+               X_RegisterVars_f,
+               X_Init_f,
+               X_Start_f,
+               X_Frame_f,
+               X_Done_f );
     return 0;
 }
