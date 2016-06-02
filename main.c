c50b2f6fecee495ca7740710a2a8e2b44a8c6af5
zloedi <stoiko.todorov@gmail.com>
2016-06-02 14:32:25 +0300

    inital setup.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <zhost.h>
+
+int main( int argc, char *argv[] ) {
+    UT_RunApp( NULL, 
+               NULL,
+               NULL,
+               colorrgb( 0.1f, 0.1f, 0.1f ),
+               NULL,
+               NULL,
+               NULL,
+               NULL,
+               NULL );
+    return 0;
+}
