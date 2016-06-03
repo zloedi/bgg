b81a5591803c8e468f20c98783ca9c9efb1801e9
zloedi <stoiko.todorov@gmail.com>
2016-06-03 18:00:51 +0300

    texture loading and blitting wip.

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "zhost.h"
+ 
+static rImage_t *x_cp437Texture;
 
 static void X_RegisterVars_f( void ) {
 }
 
 static void X_Init_f( void ) {
+    x_cp437Texture = R_LoadTexture( "cp437_12x12.png" );
 }
 
-static void X_Frame_f( void ) {
+static void X_Frame_f( const utFrameParams_t *params ) {
+    R_DrawPicV2( params->cursorPosition, v2xy( 20, 20 ), v2zero, v2one, x_cp437Texture );
 }
 
 static void X_Done_f( void ) {
