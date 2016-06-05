a73ad0b93b6ddf2f861b180262b5bb6a475c4609
stoiko <stoiko.todorov@gmail.com>
2016-06-05 20:57:46 +0300

    mouse cursor wip.

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include "zhost.h"
  
 static rImage_t *x_cp437Texture;
+static float    x_cp437TextureWidth;
 
 static void X_DrawCursor( v2_t position ) {
+    float curDim = x_cp437TextureWidth / 16. * Maxi( R_GetWindowSize().y / 512, 1 );
+    v2_t size = v2xy( curDim, curDim );
     v2_t st0 = v2xy( 10 / 16., 13 / 16. );
     v2_t st1 = v2xy( 11 / 16., 14 / 16. );
-    float sz = R_GetWindowSize().y / 44;
-    v2_t cursorSize = v2xy( sz, sz );
-    v2_t origin = v2Sub( position, v2Scale( cursorSize, 0.5 ) );
-    R_DrawPicV2( origin, cursorSize, st0, st1, x_cp437Texture );
-    R_DrawPicV2( v2Add( origin, v2Scale( cursorSize, 0.2 ) ), cursorSize, st0, st1, x_cp437Texture );
+    v2_t origin = v2Sub( position, v2Scale( size, 0.5 ) );
+    R_DrawPicV2( origin, size, st0, st1, x_cp437Texture );
+    R_DrawPicV2( 
+            v2Add( origin, v2Scale( size, 0.25 ) ), 
+            size, 
+            st0, 
+            st1, 
+            x_cp437Texture );
 }
 
 static void X_RegisterVars_f( void ) {
@@ -21,7 +27,9 @@ static void X_Init_f( void ) {
     if ( w != h ) {
         CON_Printf( "X_Init_f: non-square code page 437 texture. Using fallback texture instead.\n" );
         x_cp437Texture = r_fallbackTexture;
+        w = 1;
     }
+    x_cp437TextureWidth = w;
 }
 
 static void X_Frame_f( const utFrameParams_t *params ) {
